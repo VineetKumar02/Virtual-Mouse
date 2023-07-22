d7b4cf74d8ef4eb9a041572bd04c4ca87f2fb21d
VineetKumar02 <vineethero02@gmail.com>
2023-07-22 23:37:47 +0530

    first commit

---
+++ eye_mouse.py
@@ -0,0 +1,45 @@
+import cv2     # pip install opencv-python
+
+import mediapipe as mp
+import pyautogui
+
+cam = cv2.VideoCapture(0)   # capture 1st video source
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks = True)
+screen_width, screen_height = pyautogui.size()  # get screen dimensions
+
+while True: # run forever
+    _, frame = cam.read()   # read watever is captued by cam
+    frame = cv2.flip(frame,1)   # flip frame vertically
+    frame_height, frame_width, _ = frame.shape   # get frame dimensions
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # change color of frame from Grayscale to RGB
+    output = face_mesh.process(rgb_frame)   # process the rgb frame
+
+    landmark_points = output.multi_face_landmarks
+
+    if landmark_points:  # if the face exists
+        landmarks = landmark_points[0].landmark   # choose the 1st face
+
+        for id, landmark in enumerate(landmarks[474:478]):  # divide eye into 4 landmarks
+            x = int(landmark.x * frame_width )   # get position of face in width
+            y = int(landmark.y * frame_height )   # get position of face in height
+            cv2.circle(frame, (x,y), 3, (0, 255, 0))  # draw circles on frame with centre as(x,y) with radius as 3 and color as green(in rgb)
+
+            if id == 1:
+                screen_x = landmark.x * screen_width   # get position of cursor along width
+                screen_y = landmark.y * screen_height   # get position of cursor along height
+                pyautogui.moveTo(screen_x, screen_y)   # move cursor to given location
+
+        left = [landmarks[145], landmarks[159]]  # landmarks of top and bottom lids of left eye
+
+        for landmark in left:
+            x = int(landmark.x * frame_width )  # get position of face in width
+            y = int(landmark.y * frame_height )  # get position of face in height
+            cv2.circle(frame, (x,y), 3, (0, 255, 255))  # draw circles on frame with centre as(x,y) with radius as 3 and color as yellow(in rgb)
+
+            if(left[0].y - left[1].y < 0.004):  # get vertical postions of both landmarks(<0.004 means eyes r closed)
+                pyautogui.click()  # click
+                pyautogui.sleep(0.5)  # wait for 0.5 sec
+
+    cv2.imshow('Eye Controlled Mouse', frame)  # show frame with the window name 'Eye Controlled Mouse'
+    cv2.waitKey(1)   # wait for 1 sec
---
+++ hand_mouse.py
@@ -0,0 +1,47 @@
+import cv2
+import mediapipe as mp    # pip install mediapipe
+import pyautogui   # pip install pyautogui
+
+cap = cv2.VideoCapture(0)   # capture 1st video source
+hand_detector = mp.solutions.hands.Hands()  # detect hands
+drawing_utils = mp.solutions.drawing_utils  # An util to draw landmarks on hand
+screen_width, screen_height = pyautogui.size()  # get screen dimensions
+index_x = index_y = 0
+
+while True:   # run forever
+    _, frame = cap.read()   # read watever is captued by cam
+    frame = cv2.flip(frame,1)   # flip frame vertically
+    frame_height, frame_width, _ = frame.shape   # get frame dimensions
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # change color of frame from Grayscale to RGB
+    output = hand_detector.process(rgb_frame)   # prcoess rgb_frame and store in output
+
+    hands = output.multi_hand_landmarks   # get landmark of hands from output
+
+    if hands:   # if hand exists on screen
+        for hand in hands:   # for every point on hands
+            drawing_utils.draw_landmarks(frame, hand)  # draw the points on the frame
+            landmarks = hand.landmark
+
+            for id, landmark in enumerate(landmarks):  # divide points into several landmark
+                x = int(landmark.x * frame_width )   # get position of hand in width
+                y = int(landmark.y * frame_height )   # get position of hand in height
+
+                if id == 8:   # select index finger tip
+                    cv2.circle(img = frame, center = (x,y), radius = 10, color = (0,255,255))   # draw circle areound index finger tip
+                    index_x = landmark.x * screen_width   # get position of cursor along width
+                    index_y = landmark.y * screen_height   # get position of cursor along height
+                    pyautogui.moveTo(index_x*1.5, index_y*1.5)   # move cursor to given location
+
+                if id == 4:   # select Thumb finger tip
+                    cv2.circle(img = frame, center = (x,y), radius = 10, color = (0,255,255))   # draw circle areound index finger tip
+                    thumb_x = landmark.x * screen_width   # get position of cursor along width
+                    thumb_y = landmark.y * screen_height   # get position of cursor along height
+
+                    if(abs(index_y - thumb_y)<30 and abs(index_x - thumb_x)<30):   # if both points touch
+                        # print("click")
+                        pyautogui.click()  # click
+                        pyautogui.sleep(0.5)  # wait for 0.5 sec
+
+    cv2.imshow('Hand Mouse', frame)   # show frame with the window name 'Hand Mouse'
+    cv2.waitKey(1)   # wait for 1 sec
